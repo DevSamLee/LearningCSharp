3722207d8b407cd4fc95773e06f43ddcf8991587
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-02-02 16:38:13 -0600

    Update Program.cs

--- Overview/VarsAndData/Program.cs
+++ Overview/VarsAndData/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace VarsAndData
 {
@@ -16,23 +16,33 @@ namespace VarsAndData
             // Declare a string - it's a collection of characters
             string str = "a string";
 
-            // Declare an implicit variable
+            // Declare an implicit variable - compiler figure the type of value out
             var x = 10;
             var z = "Hello!";
 
             // TODO: Declare an array of values
+            int[] vals = new int[5];
+            string[] strs = {"one", "two", "threee" };
 
 
             // TODO: Print the values using a Formatting String
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}", i, c, b, str, f, d, x, z);
 
 
             // TODO: "null" means "no value"
-
+            object obj = null;
+            //Console.WriteLine(obj);
 
             // TODO: Implicit conversion between types
-
+            long bignum;
+            bignum = i;
 
             // TODO: Explicit conversions
+            float i_to_f = (float)i;
+            Console.WriteLine("{0}", i_to_f);
+
+            int f_to_i = (int)f;
+            Console.WriteLine("{0}", f_to_i);
 
         }
     }
