ec545b0597c8970e6935aa9b642957d8876e2e9b
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-02-02 17:00:08 -0600

    Update Program.cs

--- Overview/Comments/Program.cs
+++ Overview/Comments/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Comments
 {
@@ -6,10 +6,24 @@ namespace Comments
     {
         /// XML Comments are used to help provide documentation
         /// They start with triple-slashes and have a special syntax
+        /// <summary>
+        /// This is the main sample application function.
+        /// </summary>
+        /// <param name="args">An array of string arguments from the command line</param>
+        /// <returns>
+        /// No return value
+        /// </returns>
         static void Main(string[] args)
         {
-
+            // Single line comments start with two slashes
+            // You can have as many of these as you want
             Console.WriteLine("Hello World!");
+
+            /*
+             * Multiple line comments start with a slash and a star
+             * and can continue for several lines
+             * until a closing star and slash are encounted.
+             */
 
         }
     }
