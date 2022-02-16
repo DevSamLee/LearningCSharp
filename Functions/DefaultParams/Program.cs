447dd747b963d790632490e4444b53a11b9253b3
Sam Lee <96956309+DevSamLee@users.noreply.github.com>
2022-02-15 22:48:29 -0600

    Update Program.cs

--- Functions/DefaultParams/Program.cs
+++ Functions/DefaultParams/Program.cs
@@ -1,13 +1,14 @@
-﻿using System;
+using System;
 
 // TODO: Functions can provide default values for their parameters
-void PrintWithPrefix(string thestr, string prefix)
+void PrintWithPrefix(string thestr, string prefix="")
 {
     Console.WriteLine($"{prefix} {thestr}");
 }
 
 // TODO: Test the default parameters
-
+PrintWithPrefix("Hello, I am Sam.");
+PrintWithPrefix("Hello, I am Sam.", "::>");
 
 // TODO: Call with named params
-
+PrintWithPrefix(prefix: "% ", thestr: "Hello, I am Sam.");
